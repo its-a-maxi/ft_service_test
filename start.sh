68c49ad04bc2941011fabdaf8452ed9c3a729945
Maxi <mmonroy-@Maxis-iMac.local>
2020-11-16 12:59:58 +0100

    starting wordpress

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 minikube --vm-driver=hyperkit start --extra-config=apiserver.service-node-port-range=1-35000
 #minikube --vm-driver=docker start --extra-config=apiserver.service-node-port-range=1-35000
 minikube addons enable ingress
+minikube addons enable metallb
 minikube addons enable dashboard
 minikube dashboard &
 eval $(minikube docker-env)
 docker build -t service_nginx ./srcs/nginx
-kubectl create -f ./srcs/
+#kubectl create -f ./srcs/
